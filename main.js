55457142cbce69ecfd4dfd3c17be61c3233623e4
Ryzee119 <wendland@live.com.au>
2024-11-16 12:30:47 +1030

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
-//
-// Place any custom JS here
-//
+function generateHex(length, elementId) {
+    const hexCharacters = '0123456789ABCDEF';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+        result += hexCharacters.charAt(Math.floor(Math.random() * hexCharacters.length));
+    }
+    document.getElementById(elementId).value = result;
+}
+
+function generateDigits(length, elementId) {
+    const digitCharacters = '0123456789';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+        result += digitCharacters.charAt(Math.floor(Math.random() * digitCharacters.length));
+    }
+    document.getElement
